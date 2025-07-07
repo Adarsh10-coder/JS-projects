65cef79ab2abdfaeaab224a9568256e94de573fc
Adarsh10-coder <btwadarsh1018@gmail.com>
2025-07-07 18:25:41 +0530

    weather app done

--- 8_Weather_app/weather.js
+++ 8_Weather_app/weather.js
@@ -1,9 +1,63 @@
-const API_key = "1c523658c22fec90338d5e88ec2bc900";
-const API_url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+const key = "202732ce0ffb70a524e54c1cff779eb5";
+const url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function weatherCheck(){
-    const response = await fetch(API_url + `&appid=${API_key}`);
-    var data = await response.json();
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
-    console.log(data);
+async function weatherCheck(city){
+    const response = await fetch(url + city + `&appid=${key}`);
+
+    if(response.status == 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }
+    else{
+        var data = await response.json();
+    
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    
+        if(data.weather[0].main == "Clouds"){
+            weatherIcon.src = "images/clouds.png";
+        }
+        else if(data.weather[0].main == "Clear"){
+            weatherIcon.src = "images/clear.png";
+        }
+        else if(data.weather[0].main == "Rain"){
+            weatherIcon.src = "images/rain.png";
+        }
+        else if(data.weather[0].main == "Drizzle"){
+            weatherIcon.src = "images/drizzle.png";
+        }
+        else if(data.weather[0].main == "Mist"){
+            weatherIcon.src = "images/mist.png";
+        }
+        else if(data.weather[0].main == "Snow"){
+            weatherIcon.src = "images/snow.png";
+        }
+
+    
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+    }
 }
+
+searchBtn.addEventListener("click", () =>{
+   const city = searchBox.value.trim();
+  if (city !== "") {
+    weatherCheck(city);
+  }
+});
+searchBox.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    const city = searchBox.value.trim();
+    if (city !== "") {
+      weatherCheck(city);
+    }
+  }
+});
+
+
